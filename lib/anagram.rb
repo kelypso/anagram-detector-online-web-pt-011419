4ab12483bc42863b8c1c165ad4ac9f5b7dfccdf0
Kelsey Low <kklowlow@gmail.com>
2019-02-23 03:26:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,14 @@
 class Anagram 
+  attr_accessor :word
+  
   def initialize(word)
     @word = word
   end
 
   def match(anagrams)
+    # takes an array of possible anagrams
+    # returns all matches in an array
+    # if no matches exist, return an empty array
     
   end
 end
