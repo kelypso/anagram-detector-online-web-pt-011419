c9918d510f4aebcfca329d724d007f22e252c883
Kelsey Low <kklowlow@gmail.com>
2019-02-23 03:39:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,9 +9,7 @@ class Anagram
     # takes an array of possible anagrams
     # returns all matches in an array
     # if no matches exist, return an empty array
-    word = @word.split
-    result = []
-    anagrams.each do |w|
+    anagrams.select do |w|
       array = w.split
       result = w if array == word
     end
@@ -22,41 +20,7 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 =begin
-class Anagram
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
-  end
-
   def match(array)
     array.select do |word|
       word.split("").sort == @word.split("").sort
